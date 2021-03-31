043ae865ddf9cf3d9a7365ab367661a024ee0724
cyberimp <kinzo.x.beato@gmail.com>
2021-03-31 23:34:11 +0300

    added class to logo_watch.js

--- public/javascripts/logo_watch.js
+++ public/javascripts/logo_watch.js
@@ -3,9 +3,9 @@ let socket = new WebSocket("ws://" + host + "/socket");
 let logo = document.getElementById("logo");
 let hero = document.getElementById("hero");
 socket.onmessage = (msg) => {
-   let message = JSON.parse(msg.data);
-   logo.src = message.logo;
-   logo.classList.remove("left", "right", "center", "absolute-center");
-   logo.classList.add(message.align);
-   hero.src = message.hero;
+    let message = JSON.parse(msg.data);
+    logo.src = message.logo;
+    logo.classList.remove("left", "right", "center", "absolute-center", "left-stretch");
+    logo.classList.add(message.align);
+    hero.src = message.hero;
 };
